3e526362c65e48ba44cc4c87fc0ad15b96ed94d3
Daniel Fechner <daniel.fechner81@gmail.com>
2026-06-22 08:07:47 +0200

    remove files workshop module

--- workshop_cli_module.py
+++
@@ -1,7 +0,0 @@
-def main():
-    """Hauptfunktion des Moduls"""
-    print("CLI Module is starting")
-
-# Prüft, ob das Skript direkt aufgerufen wurde
-if __name__ == "__main__":
-    main()
--- workshop_module.py
+++
@@ -1,5 +0,0 @@
-def say_hello():
-    print("Hello")
-
-def say_bye():
-    print("Bye")
